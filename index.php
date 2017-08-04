7e1c80d2badd075bea0aafb7b2eb4b06d15ae876
Xavi Moreno <xaviermorenoaviles@gmail.com>
2017-08-03 19:56:22 -0500

    agregando codigo

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>MI mundo</title>
+        <title>mi mundo</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
-    
+    aqui hay algo que mostrar
     </body>
 </html>
 
